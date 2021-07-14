6fa47cc73fd7ee50e5cda36a9f2f69e878f8b981
RohitBh-GT <67046112+RohitBh-GT@users.noreply.github.com>
2021-07-14 20:19:47 +0530

    SignIn/SignUp completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,51 @@
-export const getUsers = (req,res) => {
-   res.send('Auth');
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import Auth from '../models/auth.js';
+
+export const getUsers = async(req,res) => {
+   try {
+       const users = await Auth.find();
+       res.status(200).json(users);
+   } catch (error) {
+       res.status(404).json({message:"No user found"});
+   }
 }
 
-export const signUp = (req,res) => {
+export const signUp = async(req,res) => {
+   const { userName,email,password } = req.body;
+   try {
+       const existingUser = await Auth.findOne({email});
 
+       if(existingUser) 
+          return res.status(404).json({message:'User already exists'});
+
+       const hashedPassword = await bcrypt.hash(password,12);
+        
+       const result = await Auth.create({userName,email,password:hashedPassword,time:new Date()});
+       const token = jwt.sign({email:result.email,time:result.time},"bingoSecretKey",{expiresIn:'1h'});
+       res.status(201).json({result,token});
+
+   } catch (error) {
+       console.log('Error while signing up');
+       res.status(409).json({message:error});
+   }
 }
 
-export const signIn = (req,res) => {
-    
+export const signIn = async(req,res) => {
+   const { email,password } = req.body;
+   try {
+       const existingUser = await Auth.findOne({email});
+       if(!existingUser)
+          return res.status(404).json({message:'User not exists'});
+
+          const confirmPassword = await bcrypt.compare(password,existingUser.password);
+          if(!confirmPassword) return res.status(400).json({message:'Incorrect credentials'});
+       
+       const token = jwt.sign({email,time:existingUser.time},"bingoSecretKey",{expiresIn:'1h'});
+       res.status(200).json({result:existingUser,token});   
+
+   } catch (error) {
+       console.log('Error while signing in');
+       res.status(409).json({message:error});
+   }
 }
