6084fc908385f7c05fc9e1c256b99311833003aa
RohitBh-GT <67046112+RohitBh-GT@users.noreply.github.com>
2021-07-17 10:54:03 +0530

    Made Object for SignIn/SignUp

--- client/src/components/Authentication/auth.jsx
+++ client/src/components/Authentication/auth.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,7 +12,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import useStyles from "./styles.js";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 
 function Copyright() {
   return (
@@ -25,97 +26,110 @@ function Copyright() {
 }
 
 
-export default function Auth({type}) {
+export default function Auth({ type }) {
   const classes = useStyles();
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [userData,setUserData] = useState({
+    email:'',userName:'',password:''
+  });
 
   const handleClick = (e) => {
-      if(type === "SignIn")
-      {
-          history.push("/Home");
-      }
-      else 
-      {
-          history.push("/SignIn");
-      }
+    if (type === "SignIn") {
+      history.push("/Home");
+    }
+    else {
+      history.push("/SignIn");
+    }
+  }
+
+  const handleChange = (e) => {
+     var {name,value} = e.target;
+     setUserData(prevVal =>{
+         return {...prevVal,[name]:value};
+     });
+     console.log(name,value);
   }
 
   return (
-      <div  className={classes.root}>
-    <Container  maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          {type}
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-         {type === "SignUp" ?  <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoFocus
-          />: null}
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-         {type === "SignIn"? <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />: null} 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={handleClick}
-          >
-            Submit
-          </Button>
-          <Grid container>
-            {type === "SignIn" ?<Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>: null}
-             {type === "SignIn"? <Link href="/SignUp" variant="body2">
+    <div className={classes.root}>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            {type}
+          </Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              value={userData.email}
+              onChange={handleChange}
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+            {type === "SignUp" ? <TextField
+              variant="outlined"
+              margin="normal"
+              onChange={handleChange}
+              required
+              fullWidth
+              value={userData.userName}
+              id="userName"
+              label="Username"
+              name="userName"
+            /> : null}
+            <TextField
+              variant="outlined"
+              margin="normal"
+              onChange={handleChange}
+              value={userData.password}
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+            />
+            {type === "SignIn" ? <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            /> : null}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleClick}
+            >
+              Submit
+            </Button>
+            <Grid container>
+              {type === "SignIn" ? <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid> : null}
+              {type === "SignIn" ? <Link href="/SignUp" variant="body2">
                 {"Don't have an account? Sign Up"}
-              </Link>: <Link href="/SignIn" className={classes.link}>
+              </Link> : <Link href="/SignIn" className={classes.link}>
                 {"Already a User Sign In !!"}
-              </Link>} 
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
+              </Link>}
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import { applyMiddleware,compose,createStore } from "redux";
-import {thunk} from "redux-thunk";
-
+import thunk from "redux-thunk";
 import App from "./App.js";
-import reducers from "./reducers";
+import {reducers} from "./reducers";
+
+
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(<Provider store={store}>
