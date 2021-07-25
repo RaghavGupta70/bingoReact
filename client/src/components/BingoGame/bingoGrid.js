d485032cca238001156c99e9f1b3db4b1994bd82
RohitBh-GT <67046112+RohitBh-GT@users.noreply.github.com>
2021-07-25 21:00:38 +0530

    Made Number cut

--- client/src/components/BingoGame/bingoGrid.js
+++ client/src/components/BingoGame/bingoGrid.js
@@ -1,24 +1,22 @@
-import react,{useState} from "react"
+import react, { useState } from "react"
 import { Container, Grid } from "@material-ui/core";
 import GridLayout from 'react-grid-layout';
-import {FaSlash} from 'react-icons/fa';
+import { FaSlash } from 'react-icons/fa';
 
 const BingoGrid = () => {
 
     var arr = [];
-    for(var i=1;i<=25;i++)
-    {
-        arr.push(i);
-    }
 
-    const [styleToggle,setStyleToggle] = useState(false);
+    const [styleToggle, setStyleToggle] = useState([]);
+    for (var i = 1; i <= 25; i++) {
+        styleToggle.push(false);
+    }
     // return(
     //     <Grid container>
     //        { arr.map((row,index) => (<Grid key={index} item xs={20%}>{row}</Grid>))}
     //     </Grid>
     // )
-
-    var obj ={
+    var obj = {
         i: "",
         x: 0,
         y: 0,
@@ -26,21 +24,16 @@ const BingoGrid = () => {
         h: 2,
     }
 
-    for(var t=0;t<25;t++)
-    {
+    for (var t = 0; t < 25; t++) {
 
-        obj={
-            i: (t+1).toString(),
-            x: (t)%5,
+        obj = {
+            i: (t + 1).toString(),
+            x: (t) % 5,
             y: 0,
             w: 1,
             h: 1,
         }
-        arr[t]=obj;
-    }
-
-    const handleClick = () => {
-        setStyleToggle(true);
+        arr[t] = obj;
     }
     // const layout = [
     //     {i: 'a', x: 0, y: 0, w: 1, h: 1},
@@ -53,19 +46,32 @@ const BingoGrid = () => {
     //   ];
 
     // arr[7]={i:"abc",x:1,y:1,w:1,h:1};
-    console.log(arr);
-      return (
+    return (
         <GridLayout className="layout" layout={arr} cols={12} colHeight={20} rowHeight={30} width={500} isDraggable={false}>
-         {/* <div key="a">a</div>
+            {/* <div key="a">a</div>
         <div key="b">b</div>
         <div key="c">c</div>
         <div key="d">d</div>
         <div key="e">e</div>
         <div key="f">f</div> */}
-        {arr.map((ar) => (<div style={{display: "flex",justifyContent: "center"}} key={ar.i} onClick={handleClick}>{ar.i}<FaSlash {{styleToggle}?style={{position: "absolute",left: "7px",fontSize: "18px"}}: null}/></div>))}
+            {arr.map((ar, index) => (<div style={{ display: "flex", justifyContent: "center" }} key={ar.i}
+                onClick={(e) => {
+                    var newArr = [];
+                    for (var p = 0; p < 25; p++) {
+                        if(styleToggle[p])
+                          newArr[p] = true;
+                        else if(p === index)
+                            newArr[p] = true;
+                        else    
+                            newArr[p] = false;
+                    }
+                    setStyleToggle(newArr);
+                }} >
+                {ar.i}<FaSlash style={{ position: "absolute", left: "7px", fontSize: "18px", visibility: styleToggle[index] ? "visible" : "hidden" }} />
+                </div>))}
 
         </GridLayout>
-      )
+    )
 }
 
 export default BingoGrid;
