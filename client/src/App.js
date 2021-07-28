f1a083a74a35f1cbb91728dca86ce638f16eb2b1
Raghav Gupta <raghavnhn@gmail.com>
2021-07-28 18:31:48 +0530

    home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import React,{useState} from "react";
-import {BrowserRouter as Router,
+import React,{useState,useEffect} from "react";
+import {withRouter,
 Switch,
 Link,
-Route} from "react-router-dom";
+Route,useHistory} from "react-router-dom";
 
 import Auth from "./components/Authentication/auth.jsx";
 import Home from "./components/main/home/home"
 
 const App = () => {
 
-    
+   const history = useHistory(); 
   const [token, setToken] = useState(JSON.parse(localStorage.getItem("tok")));
 
+  useEffect(() => {
+      history.push("/Home");
+  },[])
+
     return (
-    <Router>
         <Switch>
             <Route path="/SignIn"><Auth type={"SignIn"} setToken={setToken} /></Route>
             <Route path="/SignUp"><Auth type={"SignUp"} setToken={setToken} /></Route>
             <Route path="/Home"><Home token={token} /></Route>
         </Switch>
-    </Router>)
+    )
 }
 
-export default App;
+export default withRouter(App);
