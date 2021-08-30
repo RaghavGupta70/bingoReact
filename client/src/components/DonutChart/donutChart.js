d358dbf17e696815faa8a86dd5533922ca02106e
Raghav Gupta <raghavnhn@gmail.com>
2021-08-30 12:15:51 +0530

    .

---
+++ client/src/components/DonutChart/donutChart.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Doughnut} from 'react-chartjs-2';
+
+const DonutChart = () => {
+
+    const data = {
+      labels: ["Won", "Lost", "No Result"],
+      datasets: [
+        {
+          label: "Statistics",
+          data: [300, 50, 100],
+          backgroundColor: [
+            "rgb(255, 99, 132)",
+            "rgb(54, 162, 235)",
+            "rgb(255, 205, 86)",
+          ],
+          hoverOffset: 10,
+          cutout: '70%',
+        },
+      ],
+    };
+
+    const options = {
+      responsive: true,
+      maintainAspectRatio: true,
+      elements: {
+        arc: {
+          borderWidth: 0,
+        },
+      },
+      plugins: {
+        tooltip: {
+          enabled: true,
+        },
+        legend: {
+          display: true,
+          position: "bottom",
+          align: "center",
+          maxHeight: 100,
+          maxWidth: 400,
+          labels: {
+            boxWidth: 20,
+          },
+        },
+      },
+    };
+    return (
+        <Doughnut data={data} options={options} />
+    )
+}
+
+export default DonutChart;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ProfileBox from "../../components/ProfileBox/profileBox";
+import DonutChart from "../../components/DonutChart/donutChart";
+
+
+
 
 const Profile = () => {
   return (
-    <>
-     <ProfileBox />
-    </>
+    <div>
+      <div>
+        <ProfileBox />
+      </div>
+      <div>
+        <DonutChart />
+      </div>
+    </div>
   );
 };
 
