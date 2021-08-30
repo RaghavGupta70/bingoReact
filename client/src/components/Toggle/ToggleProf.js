ddfea31733379eb6e2cf616ff04243160176dbce
Raghav Gupta <raghavnhn@gmail.com>
2021-08-30 14:01:45 +0530

    .

---
+++ client/src/components/Toggle/ToggleProf.js
@@ -0,0 +1,35 @@
+import React,{useState} from "react";
+import ToggleButton from "@material-ui/lab/ToggleButton";
+import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
+import { FcBarChart, FcLineChart, FcPieChart } from "react-icons/fc";
+
+
+const ToggleProf = ({tog}) => {
+  const [alignment, setAlignment] = useState("left");
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+    tog(newAlignment);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+    >
+      <ToggleButton value="left" aria-label="left aligned">
+        <FcBarChart />
+      </ToggleButton>
+      <ToggleButton value="center" aria-label="centered">
+        <FcLineChart />
+      </ToggleButton>
+      <ToggleButton value="right" aria-label="right aligned">
+        <FcPieChart />
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
+
+export default ToggleProf;
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import ProfileBox from "../../components/ProfileBox/profileBox";
 import DonutChart from "../../components/DonutChart/donutChart";
-
-
-
+import PrStyles from "./Profile.module.css";
+import BarChart from "../../components/BarChart/barChart";
+import ToggleProf from "../../components/Toggle/ToggleProf";
+import LineChart from "../../components/LineChart/lineChart";
 
 const Profile = () => {
+  const [graph, setGraph] = useState("left");
   return (
-    <div>
-      <div>
-        <ProfileBox />
+    <div className={PrStyles.main_container}>
+      <div className={PrStyles.section1}>
+        <div className={PrStyles.profileBox}>
+          <ProfileBox />
+        </div>
+        <div className={PrStyles.playerStats}>
+          <div className={PrStyles.donutChart}>
+            <DonutChart />
+          </div>
+        </div>
       </div>
-      <div>
-        <DonutChart />
+      <div className={PrStyles.globalStats}>
+        <div className={PrStyles.toggle}>
+          <ToggleProf tog={setGraph} />
+        </div>
+        {graph === "left" ? (
+          <>
+            <div className={PrStyles.barChart}>
+              <BarChart />
+            </div>
+          </>
+        ) : graph === "center" ? (
+          <div className={PrStyles.lineChart}>
+            <LineChart />
+          </div>
+        ) : (
+          <div className={PrStyles.pieChart}>hello</div>
+        )}
       </div>
     </div>
   );
