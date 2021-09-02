9be4c385b8c8c1e905b59fb04143106239751f45
RohitBh-GT <67046112+RohitBh-GT@users.noreply.github.com>
2021-09-02 09:44:23 +0530

    .

--- client/src/components/main/home/home.jsx
+++ client/src/components/main/home/home.jsx
@@ -14,23 +14,6 @@ function Home({token}) {
 
    const user = JSON.parse(localStorage.getItem('user'));
    const person = user.result.userName;
-
-   var bingoNum = [];
-   for(var h=0;h<25;h++){
-       bingoNum.push(h+1);
-   }
-
-   function shuffleArray(array) {
-       for (let i = array.length - 1; i > 0; i--) {
-           const j = Math.floor(Math.random() * (i + 1));
-           [array[i], array[j]] = [array[j], array[i]];
-       }
-   }
-
-   function generator() {
-     const num = Math.floor((Math.random() * 25)+1);
-     return num;
-   }
    
    return(
        <>
