d8e353c227179c60816d9c42c10710f848ae06cb
Raghav Gupta <raghavnhn@gmail.com>
2021-09-23 14:33:31 +0530

    .

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -7,4 +7,4 @@ export const fetchUsers= () => axios.get(url);
 export const signUpUser= (data) => axios.post(`${url}/signUp`,data);
 export const signInUser= (data) => axios.post(`${url}/signIn`,data);
 export const googleSignInUser= (data) => axios.post(`${url}/googleSignIn`,data);
-export const getProfile = (data) => axios.get(`${profileURl}/profile/getMyProfile`,data);
+export const getProfile = (data) => axios.get(`${profileURl}/profile/getMyProfile/${data}`);
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,7 +3,7 @@ import { getProfile,createProfile } from '../controllers/profile.js';
 
 const router = express.Router();
 
-router.get('/getMyProfile',getProfile);
+router.get('/getMyProfile/:email',getProfile);
 router.post('/createProfile',createProfile);
 
 export default router;
