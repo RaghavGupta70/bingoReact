c132cd61a9d1437ec4848d63ce6408008fb295d2
Raghav Gupta <raghavnhn@gmail.com>
2021-10-12 10:16:49 +0530

    .

---
+++ client/src/pages/PrivacyPolicy/privacyPolicy.js
@@ -0,0 +1,139 @@
+import react from 'react'
+
+const Privacy = () => {
+    return(<>
+    <div>
+        <div>
+            <h1>
+                Bingo Privacy Policy
+            </h1>
+            <p>
+                    Policy Effective: Jan 1 2020
+
+                    This Privacy Policy describes the personal data we collect and why, how we use that data and your rights.
+
+                    This Privacy Policy applies to Absolute Games' games, websites and related services, which we here collectively call the “Service”. This Privacy Policy may be periodically updated. If we make any material changes, we will notify you by posting a notice in the Service prior to the change becoming effective. Your continued use of the Service after the effective date will be subject to the new Privacy Policy.
+
+                    For purposes of data protection laws, Absolute Games LLC. is the “data controller” of your personal information. We will refer to countries in the European Union (EU) and the European Economic Area (EEA) and Switzerland collectively as the “European Countries” in this Privacy Policy.
+            </p>
+            <div>
+                <h2>
+                    Contact Us
+                </h2>
+
+                <p>
+                        If you have any questions about data protection, or if you have any requests for resolving issues with your personal data, please contact us inside the game as that is usually the fastest channel of response.
+
+                        Binod Bingo
+                        Red Light Area, UIET Block 1,UIET
+                        Sector 25, Chandigarh
+                        email: sukhandeo@gmail.com
+                </p>
+            </div>
+            <div>
+                <h2>
+                    The Data We Collect
+                </h2>
+
+                <p>
+                    <ol type="A">
+                            <li><h1>Data you provide us:</h1>
+                            <ul>
+                                <li>
+                                        Contact information (such as name and email address)
+                                </li>
+                                    <li>
+                                        Your messages to the customer support
+                                    </li>
+                                    <li>
+                                        Other data you choose to give us (such as data to transfer progress or identify a lost account)
+                                    </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h1>
+                                    Data we collect automatically:
+                            </h1>
+                            <ul>
+                                <li>
+                                        Data about your account and game progress
+                                </li>
+                                <li>
+                                        Your IP address and mobile device identifiers (such as your device ID, advertising ID, MAC address, IMEI)
+
+                                </li>
+                                <li>
+                                        Data about your device, such as device name and operating system, browser type and language
+
+                                </li>
+                                <li>
+                                        Data we collect with cookies and similar technologies (see more below)
+
+                                </li>
+                                <li>
+                                        General location data
+
+                                </li>
+                                <li>
+                                        Data about your use of the Service, such as gameplay data and your interactions with other players inside the Service
+
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2>
+                                    Data we collect from our partners:
+                            </h2>
+
+                            <ul>
+                                <li>
+                                        Data we receive if you link a third party tool with the Service (such as Apple, Amazon, Facebook or Google)
+                                </li>
+                                <li>
+                                        Demographic data (such as to determine the coarse location of your IP address)
+
+                                </li>
+                                <li>
+                                        Data to fight fraud (such as refund abuse in games or click fraud in advertising)
+
+                                </li>
+                                <li>
+                                        Data from platforms that the games run on (such as to verify payment)
+
+                                </li>
+                                <li>
+                                        Data for advertising and analytics purposes, so we can provide you a better Service
+
+                                </li>
+
+                            </ul>
+                        </li>
+                        </ol>       
+                </p>
+            </div>
+            <div>
+                <h2>
+                        Why Do We Collect Your Data
+                </h2>
+
+                <p>
+                    <ol type ="A">
+                        <li>
+                            <h1>
+                                    To make the Service work
+                            </h1>
+                            <ul>
+                                    <h2>We process data necessary to:</h2>
+                                    <li></li>
+                            <ul>
+                        </li>
+                    </ol>
+                </p>
+            </div>
+        </div>
+    </div>
+    </>
+    )
+}
+
+export default Privacy;
