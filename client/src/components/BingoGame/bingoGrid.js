7305b7e72b58037773d4c7f2661af0a149ac0517
Raghav Gupta <raghavnhn@gmail.com>
2021-10-12 22:19:48 +0530

    .

--- client/src/components/BingoGame/bingoGrid.js
+++ client/src/components/BingoGame/bingoGrid.js
@@ -5,16 +5,21 @@ import { Container, Grid } from "@material-ui/core";
 import GridLayout from "react-grid-layout";
 import { FaSlash } from "react-icons/fa";
 import { io } from "socket.io-client";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import { getUserName, getUsers } from "../../utils/commonData/common";
-import {useHistory} from 'react-router-dom';
-import WinnerGif from '../../assets/images/winnerGif.gif';
-import alertImg from '../../assets/images/bingo_bg.png'
+import { useHistory } from "react-router-dom";
+import WinnerGif from "../../assets/images/winnerGif.gif";
+import alertImg from "../../assets/images/bingo_bg.png";
+import bingoB from "../../assets/images/bingoB.png";
+import bingoI from "../../assets/images/bingoI.png";
+import bingoN from "../../assets/images/bingoN.png";
+import bingoG from "../../assets/images/bingoG.png";
+import bingoO from "../../assets/images/bingoO.png";
+import binStyles from './bingoGrid.module.css';
 
 let socket;
 
 const BingoGrid = ({ arrNum }) => {
-
   const location = useLocation();
   const { roomID } = queryString.parse(location.search, {
     ignoreQueryPrefix: true,
@@ -22,9 +27,8 @@ const BingoGrid = ({ arrNum }) => {
   const history = useHistory();
   const ENDPOINT = "localhost:5000";
   let dependency = 0;
-  const [turn,setTurn] = useState(0);
-  const [bingoCut,setBingoCut] = useState({horiz: [],vert: [],diag: []})
-
+  const [turn, setTurn] = useState(0);
+  const [bingoCut, setBingoCut] = useState({ horiz: [], vert: [], diag: [] });
 
   var arr = new Array(5);
   var ar = new Array(25);
@@ -39,8 +43,8 @@ const BingoGrid = ({ arrNum }) => {
   });
   for (var i = 0; i < arr.length; i++) {
     arr[i] = new Array(5);
-}
-var h=0;
+  }
+  var h = 0;
   // useEffect(() => {
   //   socket.emit("gameValue", (gameValue), (error) => {
   //     alert("You bitch");
@@ -61,47 +65,46 @@ var h=0;
   // }, 100);
   const [styleToggle, setStyleToggle] = useState([]);
   const [shuffle, setShuffle] = useState(false);
-  
 
   for (var i = 1; i <= 25; i++) {
     styleToggle.push(false);
   }
 
-useEffect(() => {
-  if (bingoCut.horiz.length + bingoCut.vert.length + bingoCut.diag.length >=5)
-  {
-    Swal.fire({
-      title: 'Congratulations You Won',
-      color: 'red',
-      width: 600,
-      position: 'bottom',
-      padding: '3em',
-      background: `rgb(255,255,255)`,
-      backdrop: `
+  useEffect(() => {
+    if (
+      bingoCut.horiz.length + bingoCut.vert.length + bingoCut.diag.length >=
+      5
+    ) {
+      Swal.fire({
+        title: "Congratulations You Won",
+        color: "red",
+        width: 600,
+        position: "bottom",
+        padding: "3em",
+        background: `rgb(255,255,255)`,
+        backdrop: `
     rgba(0,0,0,0.4)
     url(${WinnerGif})
     center top
     no-repeat
-  `
-    })
-  }
-}, [bingoCut])
+  `,
+      });
+    }
+  }, [bingoCut]);
 
   useEffect(() => {
-      
-      console.log(getUsers()[turn],turn)
-      if(turn === getUsers().length )
-      {
-          setTurn(0);
-      }
-  }, [turn])
- 
-      if (!shuffle) {
+    console.log(getUsers()[turn], turn);
+    if (turn === getUsers().length) {
+      setTurn(0);
+    }
+  }, [turn]);
+
+  if (!shuffle) {
     setShuffle(true);
-    console.log("here",arrNum);
-   }
- 
-  console.log('Random')
+    console.log("here", arrNum);
+  }
+
+  console.log("Random");
 
   var obj = {
     i: "",
@@ -112,7 +115,7 @@ useEffect(() => {
   };
 
   for (var t = 0; t < 5; t++) {
-    for(var j=0;j<5;j++){
+    for (var j = 0; j < 5; j++) {
       obj = {
         i: arrNum[h].toString(),
         x: h % 5,
@@ -124,138 +127,131 @@ useEffect(() => {
       h++;
     }
   }
-  
+
   for (var t = 0; t < 25; t++) {
-      obj = {
-        i: arrNum[t].toString(),
-        x: t % 5,
-        y: 0,
-        w: 1,
-        h: 1,
-      };
-      ar[t] = obj;
-    }
-  
-  console.log(arr)
+    obj = {
+      i: arrNum[t].toString(),
+      x: t % 5,
+      y: 0,
+      w: 1,
+      h: 1,
+    };
+    ar[t] = obj;
+  }
+
+  console.log(arr);
 
   const handleBingo = (e) => {
     e.preventDefault();
-    for(let i=0;i<25;i++){
-      if(i%5===0 && !bingoCut.horiz.find((elem)=> elem===i)){
-         if(styleToggle[i]&&styleToggle[i+1]&&styleToggle[i+2]&&styleToggle[i+3]&&styleToggle[i+4]){
-           console.log(i,'You can cut now');
-           setBingoCut({horiz: [...bingoCut.horiz,i],vert: [...bingoCut.vert],diag: [...bingoCut.diag]});
-           console.log(bingoCut.horiz.length)
-         }
+    for (let i = 0; i < 25; i++) {
+      if (i % 5 === 0 && !bingoCut.horiz.find((elem) => elem === i)) {
+        if (
+          styleToggle[i] &&
+          styleToggle[i + 1] &&
+          styleToggle[i + 2] &&
+          styleToggle[i + 3] &&
+          styleToggle[i + 4]
+        ) {
+          console.log(i, "You can cut now");
+          setBingoCut({
+            horiz: [...bingoCut.horiz, i],
+            vert: [...bingoCut.vert],
+            diag: [...bingoCut.diag],
+          });
+          console.log(bingoCut.horiz.length);
+        }
       }
     }
-    for(let i=0;i<5;i++){
-      if(styleToggle[i]&&styleToggle[i+5]&&styleToggle[i+10]&&styleToggle[i+15]&&styleToggle[i+20]){
-        console.log(i,'You can cut now');
-        setBingoCut({ horiz: [...bingoCut.horiz], vert: [...bingoCut.vert,i], diag: [...bingoCut.diag] } );
-        console.log(bingoCut.vert.length)
-
-
+    for (let i = 0; i < 5; i++) {
+      if (
+        styleToggle[i] &&
+        styleToggle[i + 5] &&
+        styleToggle[i + 10] &&
+        styleToggle[i + 15] &&
+        styleToggle[i + 20]
+      ) {
+        console.log(i, "You can cut now");
+        setBingoCut({
+          horiz: [...bingoCut.horiz],
+          vert: [...bingoCut.vert, i],
+          diag: [...bingoCut.diag],
+        });
+        console.log(bingoCut.vert.length);
       }
     }
-    if(styleToggle[0]&&styleToggle[6]&&styleToggle[12]&&styleToggle[18]&&styleToggle[24]){
-      console.log('You can cut now');
-      setBingoCut({ horiz: [...bingoCut.horiz], vert: [...bingoCut.vert], diag: [...bingoCut.diag,i] } );
+    if (
+      styleToggle[0] &&
+      styleToggle[6] &&
+      styleToggle[12] &&
+      styleToggle[18] &&
+      styleToggle[24]
+    ) {
+      console.log("You can cut now");
+      setBingoCut({
+        horiz: [...bingoCut.horiz],
+        vert: [...bingoCut.vert],
+        diag: [...bingoCut.diag, i],
+      });
+    }
+    if (
+      styleToggle[4] &&
+      styleToggle[8] &&
+      styleToggle[12] &&
+      styleToggle[16] &&
+      styleToggle[20]
+    ) {
+      console.log("You can cut now");
+      setBingoCut({
+        horiz: [...bingoCut.horiz],
+        vert: [...bingoCut.vert],
+        diag: [...bingoCut.diag, i],
+      });
+    }
+  };
 
-    }
-    if(styleToggle[4]&&styleToggle[8]&&styleToggle[12]&&styleToggle[16]&&styleToggle[20]){
-      console.log('You can cut now');
-      setBingoCut({ horiz: [...bingoCut.horiz], vert: [...bingoCut.vert], diag: [...bingoCut.diag, i] } );
-    }
-  } 
-  
-  
   return (
-    <div>
-      <GridLayout
-        layout={ar}
-        cols={12}
-        colHeight={20}
-        rowHeight={30}
-        width={500}
-        isDraggable={false}
-        style={{
-          backgroundColor: "rgb(247,203,45)",
-          width: "17%",
-          margin: "auto",
-        }}
-      >
-        {arr.map((row, index) =>
-          row.map((ar, ind) => (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                border: "1px solid black",
-                alignItems: "center",
-                borderRadius: "4px",
-                margin: "auto",
-              }}
-              key={ar.i}
-              onClick={(e) => {
-                if (getUsers().length === 1) {
-                  alert("You are the only one here");
-                  return;
-                }
-                console.log(styleToggle);
-                console.log(e.target, ar.i);
-                let result = getUserName();
+    <>
+      <div className={binStyles.bingo}>
+        <img src={bingoB} />
+        <img src={bingoI} />
+        <img src={bingoN} />
+        <img src={bingoG} />
+        <img src={bingoO} />
+      </div>
+      <div className={binStyles.mainGrid}>
+        <GridLayout
+          layout={ar}
+          cols={12}
+          colHeight={20}
+          rowHeight={30}
+          width={500}
+          isDraggable={false}
+          className={binStyles.gridLayout}
+        >
+          {arr.map((row, index) =>
+            row.map((ar, ind) => (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  border: "1px solid black",
+                  alignItems: "center",
+                  borderRadius: "4px",
+                  margin: "auto",
+                }}
+                key={ar.i}
+                onClick={(e) => {
+                  if (getUsers().length === 1) {
+                    alert("You are the only one here");
+                    return;
+                  }
+                  console.log(styleToggle);
+                  console.log(e.target, ar.i);
+                  let result = getUserName();
 
-                var numLen = getUsers()[0].numbers.length;
-                if (numLen === 0) {
-                  if (getUsers()[turn].userName === result) {
-                    gameValue = {
-                      userName: result,
-                      numberSelected: ar.i,
-                      roomID: roomID,
-                    };
-                    setGameValue(gameValue);
-                    socket.emit("gameValue", gameValue, (error) => {
-                      alert("You bitch");
-                    });
-                    var newArr = [];
-                    for (var p = 0; p < 25; p++) {
-                      if (styleToggle[p]) newArr[p] = true;
-                      else if (p === index * 5 + ind) newArr[p] = true;
-                      else newArr[p] = false;
-                    }
-                    console.log(ar);
-                    setStyleToggle(newArr);
-                    var f = turn + 1;
-                    setTurn(f);
-                  } else {
-                    alert("Ruk Ja Chodu");
-                  }
-                } else if (getUsers()[turn].userName === result) {
-                  gameValue = {
-                    userName: result,
-                    numberSelected: ar.i,
-                    roomID: roomID,
-                  };
-                  setGameValue(gameValue);
-                  socket.emit("gameValue", gameValue, (error) => {
-                    alert("You bitch");
-                  });
-                  var newArr = [];
-                  for (var p = 0; p < 25; p++) {
-                    if (styleToggle[p]) newArr[p] = true;
-                    else if (p === index * 5 + ind) newArr[p] = true;
-                    else newArr[p] = false;
-                  }
-                  console.log(ar);
-                  setStyleToggle(newArr);
-                  var f = turn + 1;
-                  setTurn(f);
-                } else {
-                  if (getUsers()[0].numbers[numLen - 1].userName === result) {
-                    alert("Keep Your Calm Boi");
-                  } else {
-                    if (getUsers()[0].numbers[numLen - 1].value === ar.i) {
+                  var numLen = getUsers()[0].numbers.length;
+                  if (numLen === 0) {
+                    if (getUsers()[turn].userName === result) {
                       gameValue = {
                         userName: result,
                         numberSelected: ar.i,
@@ -276,29 +272,77 @@ useEffect(() => {
                       var f = turn + 1;
                       setTurn(f);
                     } else {
-                      alert("You selected wrong number");
+                      alert("Ruk Ja Chodu");
+                    }
+                  } else if (getUsers()[turn].userName === result) {
+                    gameValue = {
+                      userName: result,
+                      numberSelected: ar.i,
+                      roomID: roomID,
+                    };
+                    setGameValue(gameValue);
+                    socket.emit("gameValue", gameValue, (error) => {
+                      alert("You bitch");
+                    });
+                    var newArr = [];
+                    for (var p = 0; p < 25; p++) {
+                      if (styleToggle[p]) newArr[p] = true;
+                      else if (p === index * 5 + ind) newArr[p] = true;
+                      else newArr[p] = false;
+                    }
+                    console.log(ar);
+                    setStyleToggle(newArr);
+                    var f = turn + 1;
+                    setTurn(f);
+                  } else {
+                    if (getUsers()[0].numbers[numLen - 1].userName === result) {
+                      alert("Keep Your Calm Boi");
+                    } else {
+                      if (getUsers()[0].numbers[numLen - 1].value === ar.i) {
+                        gameValue = {
+                          userName: result,
+                          numberSelected: ar.i,
+                          roomID: roomID,
+                        };
+                        setGameValue(gameValue);
+                        socket.emit("gameValue", gameValue, (error) => {
+                          alert("You bitch");
+                        });
+                        var newArr = [];
+                        for (var p = 0; p < 25; p++) {
+                          if (styleToggle[p]) newArr[p] = true;
+                          else if (p === index * 5 + ind) newArr[p] = true;
+                          else newArr[p] = false;
+                        }
+                        console.log(ar);
+                        setStyleToggle(newArr);
+                        var f = turn + 1;
+                        setTurn(f);
+                      } else {
+                        alert("You selected wrong number");
+                      }
                     }
                   }
-                }
-              }}
-            >
-              {ar.i}
-              <FaSlash
-                style={{
-                  position: "absolute",
-                  left: "7px",
-                  fontSize: "18px",
-                  visibility: styleToggle[index * 5 + ind]
-                    ? "visible"
-                    : "hidden",
                 }}
-              />
-            </div>
-          ))
-        )}
-      </GridLayout>
-      <button onClick={handleBingo}>Bingo</button>
-    </div>
+              >
+                {ar.i}
+                <FaSlash
+                  style={{
+                    position: "absolute",
+                    left: "7px",
+                    fontSize: "18px",
+                    visibility: styleToggle[index * 5 + ind]
+                      ? "visible"
+                      : "hidden",
+                  }}
+                />
+              </div>
+            ))
+            )}
+        </GridLayout>
+        <button onClick={handleBingo}>Bingo</button>
+      </div>
+    </>
   );
 };
 
